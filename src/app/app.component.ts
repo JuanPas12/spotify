6451567dfadbc033f14ee33390b474a152f6d788
Juan Pablo <juanptafolla@gmail.com>
2023-11-28 09:33:22 -0600

    Tipado de datos
    Tipado de datos con TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,41 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'spotify';
+
+  name: string = 'Juan Pablo'
+  age : number = 23
+  phone: null = null
+  phone1: undefined = undefined
+
+  //any permite almacenar cualquier tipo de datos
+  datum: any
+
+  car: CarModel = {
+    brand: 'Ford',
+    model: 'Focus',
+    year: 2023
+  }
+
+  //listCars:Array<any> = [1, 'hola', null, {}]
+  listCars:Array<CarModel> = [
+    {
+      brand: 'Chevrolet',
+      model: 'Camaro',
+      year: 2024
+    },
+    {
+      brand: 'Mercedes Benz',
+      model: 'AMG',
+      year: 2023
+    }
+  ]
+
+  //se pueden poner 2 tipos de datos
+  status:string | number = 'fail'
+}
+
+interface CarModel {
+  brand:string
+  model:string
+  year?:number
 }
