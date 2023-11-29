9a6852d976953e7a71c06e8de04a74cdef7e0f53
Juan Pablo <juanptafolla@gmail.com>
2023-11-29 14:21:01 -0600

    ng-template
    Uso de ng-template para la incorporación de codigo.

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrl: './media-player.component.css'
 })
-export class MediaPlayerComponent {
+export class MediaPlayerComponent implements OnInit {
+  mockCover: any = {
+    cover: 'https://jenesaispop.com/wp-content/uploads/2009/09/guetta_onelove.jpg',
+    name: 'Hola',
+    album: 'Hola'
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 }
