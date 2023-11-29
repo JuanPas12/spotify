be589609c7233938aec3978d83ae15be500fece6
Juan Pablo <juanptafolla@gmail.com>
2023-11-29 08:57:24 -0600

    routing
    Uso de routing y creación de mi primer componente

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
+
+const routes: Routes = [
+  {
+    path: '', //TODO: localhost:4200/ <---
+    component: ExampleComponent
+  },
+  {
+    path: 'hello', //TODO: localhost:4200/ <---
+    component: ExampleComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
