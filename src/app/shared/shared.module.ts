2315d251b532018e95e381d8fca288e58e7853ff
Juan Pablo <juanptafolla@gmail.com>
2023-11-30 16:27:48 -0600

    Pipe (Filtros)
    Implementación de filtros de orden en la vista de 'favorites'.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,6 +8,7 @@ import { SectionGenericComponent } from './components/section-generic/section-ge
 import { PlayListHeaderComponent } from './components/play-list-header/play-list-header.component';
 import { PlayListBodyComponent } from './components/play-list-body/play-list-body.component';
 import { RouterModule } from '@angular/router';
+import { OrderListPipe } from './pipe/order-list.pipe';
 
 
 
@@ -20,6 +21,7 @@ import { RouterModule } from '@angular/router';
     SectionGenericComponent,
     PlayListHeaderComponent,
     PlayListBodyComponent,
+    OrderListPipe,
   ],
   imports: [
     CommonModule,
@@ -33,6 +35,7 @@ import { RouterModule } from '@angular/router';
     SectionGenericComponent,
     PlayListHeaderComponent,
     PlayListBodyComponent,
+    OrderListPipe
   ]
 })
 export class SharedModule { }
