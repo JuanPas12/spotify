4686272a30a6c0d4af003c0a2b36e44cee330958
Juan Pablo <juanptafolla@gmail.com>
2023-12-06 09:15:48 -0600

    LogIn forms
    Creación de formulario para el login.

--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-page',
   templateUrl: './auth-page.component.html',
   styleUrl: './auth-page.component.css'
 })
-export class AuthPageComponent {
+export class AuthPageComponent implements OnInit {
+  formLogin: FormGroup = new FormGroup({});
+  constructor () { }
 
+  ngOnInit(): void {
+    this.formLogin = new FormGroup(
+      {
+        email:new FormControl('',[
+          Validators.required,
+          Validators.email
+        ]),
+        password: new FormControl('',[
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(10)
+        ])
+      }
+    )
+  }
+
+  sendLogin(): void {
+    const body = this.formLogin.value
+  }
 }
